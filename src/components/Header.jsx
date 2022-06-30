4318567774224e03704a8de441dd1dfb566330df
ClaytonMatos83 <clayton_matos@outlook.com.br>
2022-06-30 14:09:38 -0300

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from'./Header.module.css'
 
+import cmLogo from '../assets/cm-logo.svg';
+
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>CM Feed</strong>
+            <img src={cmLogo} alt="Logotipo do CM feed"/>
         </header>
     );
 }
