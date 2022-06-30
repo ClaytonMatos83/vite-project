a1a9edc57daf6906e17eb5e42bf6f23f90cf197f
ClaytonMatos83 <clayton_matos@outlook.com.br>
2022-06-30 12:17:39 -0300

    properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-  import {Post} from './Post';
+import {Post} from './Post';
 
 export function App() {
   return (
     <div> 
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post 
+        author="Clayton Matos" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi fuga illo repudiandae, consequuntur commodi dolorum quod debitis eveniet nemo explicabo optio quibusdam ipsa aperiam suscipit adipisci vitae maiores porro ex!"
+      />
+      <Post
+        author="Pedro Matos"
+        content="Um novo post"
+      />
     </div>
     )
 }
