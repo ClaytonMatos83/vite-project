6e904ab284cadfccdbdfa55608e37f57afdc7ea7
ClaytonMatos83 <clayton_matos@outlook.com.br>
2022-06-30 13:07:45 -0300

    css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
 import {Post} from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <div> 
+      <Header/>
+
       <Post 
         author="Clayton Matos" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi fuga illo repudiandae, consequuntur commodi dolorum quod debitis eveniet nemo explicabo optio quibusdam ipsa aperiam suscipit adipisci vitae maiores porro ex!"
